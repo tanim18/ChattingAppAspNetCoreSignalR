522356f2efc25c7faec9c202d9a61bc35c9fadfb
tanim94 <tanim.snowtexgroup@gmail.com>
2022-01-12 18:07:53 +0600

    first commit

---
+++ ChattingAppAsp.NetCoreSignalR/ChattingAppAsp.NetCoreSignalR/HubConfig/MyHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChattingAppAsp.NetCoreSignalR.HubConfig
+{
+    public class MyHub : Hub
+    {
+        public async Task askServer(string somethingTextFromClient)
+        {
+            string tempString = "";
+            if (somethingTextFromClient == "hey")
+            {
+                tempString = "message was 'hey'";
+            }
+            else
+            {
+                tempString = "message was something else";
+            }
+            await Clients.Client(this.Context.ConnectionId).SendAsync("askServerResponse", tempString);
+        }
+    }
+}
